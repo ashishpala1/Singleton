be78753617739d5e710de9089673e6d84c0b57fd
ashish.pal <apal9623@gmail.com>
2023-06-20 22:39:33 +0530

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            Single s1 = Single.Instance();
+            Single s2 = Single.Instance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Single
+    {
+        static Single single;
+
+        private Single()
+        {
+            
+        }
+
+        public static Single Instance()
+        {
+            if(single == null)
+            {
+                single = new Single();
+            }
+
+            return single;
+        }
+    }
+}
